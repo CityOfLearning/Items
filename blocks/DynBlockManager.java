b9d9cf7dd69bbd037461e10d709a26df4e898d79
Dominic Amato <dominic.a.amato@gmail.com>
2016-12-05 15:12:10 -0600

    register new blocks

--- blocks/DynBlockManager.java
+++ blocks/DynBlockManager.java
@@ -5,6 +5,14 @@ import com.dyn.fixins.blocks.cmdblock.StudentCommandBlock;
 import com.dyn.fixins.blocks.cmdblock.tileentity.StudentCommandBlockTileEntity;
 import com.dyn.fixins.blocks.dialog.DialogBlock;
 import com.dyn.fixins.blocks.dialog.DialogBlockTileEntity;
+import com.dyn.fixins.blocks.dialog.StudentDialogBlock;
+import com.dyn.fixins.blocks.redstone.proximity.ProximityBlock;
+import com.dyn.fixins.blocks.redstone.proximity.ProximityBlockTileEntity;
+import com.dyn.fixins.blocks.redstone.proximity.StudentProximityBlock;
+import com.dyn.fixins.blocks.redstone.timer.StudentTimerBlock;
+import com.dyn.fixins.blocks.redstone.timer.TimerBlock;
+import com.dyn.fixins.blocks.redstone.timer.TimerBlockTileEntity;
+import com.dyn.fixins.tab.DYNTab;
 
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
@@ -12,27 +20,57 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class DynBlockManager {
 
+	public static Block studentDialogBlock;
 	public static Block studentCmdBlock;
+	public static Block studentProximityBlock;
+	public static Block studentTimerBlock;
+
 	public static Block dialogBlock;
-	public static Block blockSimple;
+	public static Block proximityBlock;
+	public static Block timerBlock;
+
+	public static CreativeTabs dynTab = new DYNTab();
 
 	public static void load() {
 		studentCmdBlock = (new StudentCommandBlock().setBlockUnbreakable().setResistance(6000000.0F)
-				.setUnlocalizedName("student_command_block"));
+				.setUnlocalizedName("student_command_block")).setCreativeTab(dynTab);
 		GameRegistry.registerBlock(studentCmdBlock, "student_command_block");
 		GameRegistry.registerTileEntity(StudentCommandBlockTileEntity.class, "student_command_block_te");
 
-		dialogBlock = (new DialogBlock().setUnlocalizedName("dialog_block").setCreativeTab(CreativeTabs.tabMisc));
+		dialogBlock = (new DialogBlock().setUnlocalizedName("dialog_block"));
 		GameRegistry.registerBlock(dialogBlock, "dialog_block");
 		GameRegistry.registerTileEntity(DialogBlockTileEntity.class, "dialog_block_te");
 
-		// blockSimple = (BlockSimple) (new
-		// BlockSimple().setUnlocalizedName("block_test"));
-		// GameRegistry.registerBlock(blockSimple, "block_test");
+		studentDialogBlock = (new StudentDialogBlock().setUnlocalizedName("student_dialog_block")
+				.setCreativeTab(dynTab));
+		GameRegistry.registerBlock(studentDialogBlock, "student_dialog_block");
+
+		proximityBlock = new ProximityBlock().setUnlocalizedName("proximity_block");
+		GameRegistry.registerBlock(proximityBlock, "proximity_block");
+		GameRegistry.registerTileEntity(ProximityBlockTileEntity.class, "proximity_block_te");
+
+		timerBlock = new TimerBlock().setUnlocalizedName("timer_block");
+		GameRegistry.registerBlock(timerBlock, "timer_block");
+		GameRegistry.registerTileEntity(TimerBlockTileEntity.class, "timer_block_te");
+
+		studentProximityBlock = new StudentProximityBlock().setUnlocalizedName("student_proximity_block")
+				.setCreativeTab(dynTab);
+		GameRegistry.registerBlock(studentProximityBlock, "student_proximity_block");
+
+		studentTimerBlock = new StudentTimerBlock().setUnlocalizedName("student_timer_block").setCreativeTab(dynTab);
+		GameRegistry.registerBlock(studentTimerBlock, "student_timer_block");
 	}
 
 	public static void register() {
-		DynFixinsMod.proxy.registerBlockItem(studentCmdBlock);
+		// Utility Items
 		DynFixinsMod.proxy.registerBlockItem(dialogBlock);
+		DynFixinsMod.proxy.registerBlockItem(proximityBlock);
+		DynFixinsMod.proxy.registerBlockItem(timerBlock);
+
+		// Student Items
+		DynFixinsMod.proxy.registerBlockItem(studentCmdBlock);
+		DynFixinsMod.proxy.registerBlockItem(studentDialogBlock);
+		DynFixinsMod.proxy.registerBlockItem(studentProximityBlock);
+		DynFixinsMod.proxy.registerBlockItem(studentTimerBlock);
 	}
 }
