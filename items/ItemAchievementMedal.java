4b7440be5f51a28938ab469474e46a8420553982
Dominic Amato <dominic.a.amato@gmail.com>
2017-03-28 00:52:24 -0500

    achievemetn medal

---
+++ items/ItemAchievementMedal.java
@@ -0,0 +1,214 @@
+package com.dyn.fixins.items;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.swing.JTree;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import com.dyn.achievements.achievement.AchievementPlus;
+import com.dyn.schematics.Schematic;
+import com.dyn.schematics.SchematicRegistry;
+import com.dyn.schematics.SchematicRenderingRegistry;
+import com.dyn.utils.TreeNode;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Achievement;
+import net.minecraft.stats.AchievementList;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import noppes.npcs.controllers.SchematicController;
+
+public class ItemAchievementMedal extends Item {
+	private static final Map<Integer, Pair<Integer, String>> ACHIEVEMENTS = Maps
+			.<Integer, Pair<Integer, String>>newHashMap();
+	private static final Map<String, Integer> NAME_MAP = Maps
+			.<String, Integer>newHashMap();
+
+	public static int getMetaFromAchievementName(String name){
+		if(NAME_MAP.size() == 0){
+			createMappings();
+		}
+		return NAME_MAP.get(name);
+	}
+	
+	private static void createMappings(){
+		int counter = 0;
+		List<TreeNode<Achievement>> roots = Lists.newArrayList();
+
+		for (Achievement ach : AchievementList.achievementList) {
+			if (ach.parentAchievement == null) {
+				roots.add(new TreeNode<Achievement>(ach));
+				continue;
+			}
+			for (TreeNode<Achievement> root : roots) {
+				if (ach.parentAchievement == root.data) {
+					root.addChild(ach);
+				} else {
+					TreeNode<Achievement> node = root.findTreeNode(new Comparable<Achievement>() {
+						@Override
+						public int compareTo(Achievement treeData) {
+							if (treeData == null)
+								return 1;
+							boolean nodeOk = treeData == ach.parentAchievement;
+							return nodeOk ? 0 : 1;
+						}
+					});
+					if (node != null) {
+						node.addChild(ach);
+					}
+				}
+			}
+		}
+
+		for (TreeNode<Achievement> root : roots) {
+			for (TreeNode<Achievement> node : root) {
+				if (node.data instanceof AchievementPlus) {
+					//its possible that a node can be special and not a leaf
+					NAME_MAP.put(((AchievementPlus) node.data).getName(), counter);
+					ACHIEVEMENTS.put(counter++, new ImmutablePair<Integer, String>(
+							node.data.getSpecial() ? 0 
+									: Math.min(recursiveTreeSearch(node), 4),
+									((AchievementPlus) node.data).getName()));
+				} else {
+					NAME_MAP.put(node.data.getStatName().getUnformattedText(), counter);
+					ACHIEVEMENTS.put(counter++,
+							new ImmutablePair<Integer, String>(node.data.getSpecial() ? 0
+									: Math.min(recursiveTreeSearch(node), 4),
+									node.data.getStatName().getUnformattedText()));
+				}
+			}
+		}
+	}
+	
+	public ItemAchievementMedal() {
+		maxStackSize = 1;
+		setHasSubtypes(true);
+	}
+
+	/**
+	 * allows items to add custom lines of information to the mouseover
+	 * description
+	 */
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
+		tooltip.add(EnumChatFormatting.GOLD + ACHIEVEMENTS.get(stack.getItemDamage()).getRight());
+	}
+
+	/**
+	 * returns a list of items with the same ID, but different meta (eg: dye
+	 * returns 16 items)
+	 */
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void getSubItems(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
+		int counter = 0;
+		List<TreeNode<Achievement>> roots = Lists.newArrayList();
+
+		for (Achievement ach : AchievementList.achievementList) {
+			if (ach.parentAchievement == null) {
+				roots.add(new TreeNode<Achievement>(ach));
+				continue;
+			}
+			for (TreeNode<Achievement> root : roots) {
+				if (ach.parentAchievement == root.data) {
+					root.addChild(ach);
+				} else {
+					TreeNode<Achievement> node = root.findTreeNode(new Comparable<Achievement>() {
+						@Override
+						public int compareTo(Achievement treeData) {
+							if (treeData == null)
+								return 1;
+							boolean nodeOk = treeData == ach.parentAchievement;
+							return nodeOk ? 0 : 1;
+						}
+					});
+					if (node != null) {
+						node.addChild(ach);
+					}
+				}
+			}
+		}
+
+		for (TreeNode<Achievement> root : roots) {
+			for (TreeNode<Achievement> node : root) {
+				subItems.add(new ItemStack(itemIn, 1, counter));
+				if (node.data instanceof AchievementPlus) {
+					//its possible that a node can be special and not a leaf
+					NAME_MAP.put(((AchievementPlus) node.data).getName(), counter);
+					ACHIEVEMENTS.put(counter++, new ImmutablePair<Integer, String>(
+							node.data.getSpecial() ? 0 
+									: Math.min(recursiveTreeSearch(node), 4),
+									((AchievementPlus) node.data).getName()));
+				} else {
+					NAME_MAP.put(node.data.getStatName().getUnformattedText(), counter);
+					ACHIEVEMENTS.put(counter++,
+							new ImmutablePair<Integer, String>(node.data.getSpecial() ? 0
+									: Math.min(recursiveTreeSearch(node), 4),
+									node.data.getStatName().getUnformattedText()));
+				}
+			}
+		}
+	}
+
+	private static int recursiveTreeSearch(TreeNode<Achievement> node){
+		if(node.isLeaf()){
+			if(node.data.getSpecial()){
+				return 0;
+			}
+			return 1;
+		} 
+		int max = 0;
+		for(TreeNode<Achievement> child : node.children){
+			max = Math.max(max, recursiveTreeSearch(child));
+		}
+		return max+1;
+	}
+	
+	/**
+	 * Called whenever this item is equipped and the right mouse button is
+	 * pressed. Args: itemStack, world, entityPlayer
+	 */
+	@Override
+	public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn) {
+		if (worldIn.isRemote) {
+
+		}
+		return itemStackIn;
+	}
+
+	/**
+	 * Returns the unlocalized name of this item. This version accepts an
+	 * ItemStack so different stacks can have different names based on their
+	 * damage or NBT.
+	 */
+	public String getUnlocalizedName(ItemStack stack) {
+		return super.getUnlocalizedName() + "_" + ACHIEVEMENTS.get(stack.getItemDamage()).getLeft();
+	}
+
+	/**
+	 * Called when a Block is right-clicked with this Item
+	 */
+	@Override
+	public boolean onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumFacing side,
+			float hitX, float hitY, float hitZ) {
+		if (worldIn.isRemote) {
+
+		}
+		return true;
+	}
+}
